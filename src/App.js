aecda29f84d9deb61324e4c171e16b0b2cc82b7c
bc-dipesh <bcdipeshwork@gmail.com>
2022-10-23 16:55:43 -0400

    Remove API and use local data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import quotes from './quotes.json';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,25 +7,17 @@ import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge';
 
 const App = () => {
-	const API_URL = 'https://zenquotes.io/api/quotes/';
-	const [quotes, setQuotes] = useState([]);
-	const [currentQuote, setCurrentQuote] = useState(0);
-
-	const fetchQuotes = async () => {
-		try {
-			const response = await axios.get(API_URL);
-			setQuotes(response.data.map((res) => res.h));
-		} catch (err) {
-			console.log(err);
-		}
-	};
+	const [currentQuote, setCurrentQuote] = useState(
+		Math.ceil(Math.random() * quotes.length)
+	);
+	const [quotesArray, setQuotesArray] = useState([]);
 
 	const nextQuote = (e) => {
-		if (currentQuote < quotes.length - 1) {
-			setCurrentQuote((prevValue) => prevValue + 1);
-		} else {
-			window.location.reload(true);
-		}
+		setCurrentQuote(Math.ceil(Math.random() * quotes.length));
+	};
+
+	const fetchQuotes = () => {
+		setQuotesArray(quotes.map((quote) => quote.h));
 	};
 
 	useEffect(() => {
@@ -38,7 +30,7 @@ const App = () => {
 				<Col xs={12} className="align-self-center">
 					<div
 						dangerouslySetInnerHTML={{
-							__html: quotes[currentQuote],
+							__html: quotesArray[currentQuote],
 						}}
 					></div>
 					<Button className="mt-5" onClick={nextQuote}>
