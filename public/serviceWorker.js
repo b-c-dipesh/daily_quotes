065f3f6e27ceb1998a7d3344f172351bce8da842
bc-dipesh <bcdipeshwork@gmail.com>
2022-10-23 20:12:41 -0400

    Fix bug in serviceWorker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,7 +3,7 @@ const urlsToCache = ['/', '/index.html'];
 
 self.addEventListener('install', function (event) {
 	// Perform install steps
-	event.waitUntill(
+	event.waitUntil(
 		caches.open(CACHE_NAME).then(function (cache) {
 			console.log('Opened cache');
 			return cache.addAll(urlsToCache);
